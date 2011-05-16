248d61ebbcf6d00e97dd131dad1a5f2f9cbeac7f
Vittal Aithal <opensource@cognidox.com>
2011-05-16 17:51:28 +0100

    Try and work around a problem on lunarpages
    CURLOPT_FOLLOWLOCATION won't work on lunarpages, so try a work-around
    for it by manually parsing the redirections.

--- joomla_1.5/com_wordbridge/site/helpers/helper.php
+++ joomla_1.5/com_wordbridge/site/helpers/helper.php
@@ -44,11 +44,8 @@ class WordbridgeHelper {
         $url = sprintf( 'http://twitter-api.wordpress.com/users/show.xml?screen_name=%s', $blogname );
         $curl = curl_init();
         curl_setopt( $curl, CURLOPT_URL, $url );
-        curl_setopt( $curl, CURLOPT_HEADER, false );
-        curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, true );
-        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
 
-        $xml = curl_exec( $curl );
+        $xml = WordbridgeHelper::curl_redir_exec( $curl );
         curl_close( $curl );
         if ( empty( $xml ) )
         {
@@ -188,11 +185,8 @@ class WordbridgeHelper {
         // Use curl to get the data
         $curl = curl_init();
         curl_setopt( $curl, CURLOPT_URL, $url );
-        curl_setopt( $curl, CURLOPT_HEADER, false );
-        curl_setopt( $curl, CURLOPT_FOLLOWLOCATION, true );
-        curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
 
-        $xml = curl_exec( $curl );
+        $xml = WordbridgeHelper::curl_redir_exec( $curl );
         curl_close( $curl );
         if ( empty( $xml ) )
         {
@@ -336,6 +330,51 @@ class WordbridgeHelper {
             return true;
         }
         return false;
+    }
+
+    /**
+     * curl_redir_exec
+     * Work around safe_mode restrictions on CURLOPT_FOLLOWLOCATION
+     * Taken from http://php.net/manual/en/function.curl-setopt.php#71313
+     * eion at bigfoot.com
+     */
+    function curl_redir_exec( $ch, $curl_loops = 0 )
+    {
+        static $curl_max_loops = 20;
+        if ( $curl_loops++ >= $curl_max_loops )
+        {
+            return FALSE;
+        }
+        curl_setopt( $ch, CURLOPT_HEADER, true );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        $data = curl_exec( $ch );
+        list( $header, $data ) = preg_split( '/(\r?\n){2}/', $data, 2 );
+        $http_code = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+        if ( $http_code == 301 || $http_code == 302 )
+        {
+            $matches = array();
+            preg_match( '/Location:(.*?)\n/', $header, $matches );
+            $url = parse_url( trim( array_pop( $matches ) ) );
+            if ( !$url )
+            {
+                //couldn't process the url to redirect to
+                return $data;
+            }
+            $last_url = parse_url( curl_getinfo( $ch, CURLINFO_EFFECTIVE_URL ) );
+            if ( !$url['scheme'] )
+                $url['scheme'] = $last_url['scheme'];
+            if ( !$url['host'] )
+                $url['host'] = $last_url['host'];
+            if ( !$url['path'] )
+                $url['path'] = $last_url['path'];
+            $new_url = $url['scheme'] . '://' . $url['host'] . $url['path'] . ($url['query']?'?'.$url['query']:'');
+            curl_setopt( $ch, CURLOPT_URL, $new_url );
+            return WordbridgeHelper::curl_redir_exec( $ch, $curl_loops );
+        }
+        else
+        {
+            return $data;
+        }
     }
 }
 
