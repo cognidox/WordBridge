d80f7e4b92d3fa28000eabf730c7365b33416df3
Vittal Aithal <opensource@cognidox.com>
2011-08-24 16:02:39 +0100

    Make sure the SQL tables are cleared up properly

--- joomla_1.5/com_wordbridge/admin/uninstall.sql
+++ joomla_1.5/com_wordbridge/admin/uninstall.sql
@@ -3,3 +3,5 @@ DROP TABLE IF EXISTS `#__com_wordbridge_cache`;
 DROP TABLE IF EXISTS `#__com_wordbridge_pages`;
 DROP TABLE IF EXISTS `#__com_wordbridge_posts`;
 DROP TABLE IF EXISTS `#__com_wordbridge_post_categories`;
+DROP TABLE IF EXISTS `#__com_wordbridge_blog_categories`;
+DROP TABLE IF EXISTS `#__com_wordbridge_blog_tags`;
