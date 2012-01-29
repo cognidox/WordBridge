c0d587b3303e9fed4264e63b843cf4edce2473be
Vittal Aithal <opensource@cognidox.com>
2012-01-29 15:11:56 +0000

    Copy more link code from J1.6 to J1.5 (GH #22)

--- joomla_1.5/com_wordbridge/site/views/entries/tmpl/default.php
+++ joomla_1.5/com_wordbridge/site/views/entries/tmpl/default.php
@@ -44,14 +44,69 @@ require_once( JPATH_COMPONENT.DS.'helpers'.DS.'helper.php' );
                             $blogContent = substr( $entry['content'], 0, $br_pos );
                         }
                     }
+
                     // Look for more-link
-                    if ( preg_match( '/^(.+?)<span\s+id="more-(\d+)"><\/span>.*/is', $blogContent, $matches ) )
+                    if ( preg_match( '/^(.+?)<span\s+id="more-(\d+)"><\/span>(.*)/is', $blogContent, $matches ) )
                     {
                         $blogContent = $matches[1];
-                        $blogContent .= sprintf( '<a href="%s#more-%s">%s</a></p>', 
+                        $blogContent .= sprintf( '<a href="%s#more-%s">%s</a>',
                                                  JRoute::_( $this->blogLink . '&p=' . $entry['postid'] .
                                                             '&slug=' . $entry['slug'] . '&view=entry' ),
                                                  $matches[2], JText::_( 'COM_WORDBRIDGE_READ_THE_REST' ) );
+                        // Care needs to be taken to allow closing tags from 
+                        // earler in the content to close.
+                        // Strip closed block elements, which should just
+                        // leave closing elements
+                        $parts = preg_split( '/(<[^>]+>)/s', $matches[3], -1, PREG_SPLIT_NO_EMPTY|PREG_SPLIT_DELIM_CAPTURE );
+                        $tags = array();
+                        $tag_index = array();
+                        foreach ( $parts as $part )
+                        {
+                            // Skip non HTML things
+                            if ( strpos( $part, '<' ) !== 0 )
+                            {
+                                continue;
+                            }
+                            // Skip self closing elements
+                            if ( preg_match( '/\/>$/', $part ) )
+                                continue;
+                            // Skip elements that should be self closed,
+                            // but are often not
+                            if ( preg_match( '/^<(hr|br|img|input|meta|link|basefont|base|area|col|frame|param)(\s|>)/is', $part ) )
+                                continue;
+
+                            preg_match( '/^<\/?(\w+)/', $part, $pmatch );
+                            $tag = strtolower( $pmatch[1] );
+                            if ( strpos( $part, '/' ) === 1 )
+                            {
+                                // This is a closing element - if we have the
+                                // opening element in tag_index, we can
+                                // trim the tag list
+                                if ( array_key_exists( $tag, $tag_index ) )
+                                {
+                                    $pos = array_pop( $tag_index[$tag] );
+                                    $tags = array_slice( $tags, 0, $pos );
+                                    if ( count( $tag_index[$tag] ) == 0 )
+                                        unset( $tag_index[$tag] );
+                                }
+                                else
+                                {
+                                    // Add the closing tag to the tags list
+                                    $tags[] = $part;
+                                }
+                            }
+                            else
+                            {
+                                // This is an openining element - we add
+                                // it to the tags list, and store the index
+                                // position
+                                if ( !array_key_exists( $tag, $tag_index ) )
+                                    $tag_index[$tag] = array();
+                                array_push( $tag_index[$tag], count( $tags ) );
+                                $tags[] = $part;
+                            }
+                        }
+                        $blogContent .= implode( '', $tags );
                     }
                     echo $blogContent;
                 ?>
