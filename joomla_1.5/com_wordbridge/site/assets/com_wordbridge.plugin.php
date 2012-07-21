f94f32b2abc69df03e475a5b3245962d2d47ae81
Vittal Aithal <opensource@cognidox.com>
2012-07-21 17:06:33 +0100

    re GH#27 - adds support for JComments
    JComments is a Joomla component from JoomlaTune (http://www.joomlatune.com/jcomments.html)
    which lets people post up comments to Joomla objects. The update allows sites that use
    JComments to add comments to blog posts. Note that comments on posts are only stored
    in Joomla, and not passed back to WordPress.

---
+++ joomla_1.5/com_wordbridge/site/assets/com_wordbridge.plugin.php
@@ -0,0 +1,44 @@
+<?php
+
+// JComments plugin for WordBridge
+// http://www.cognidox.com/opensource/wordbridge-wordpress-to-joomla-bridge
+// (c) 2012 Cognidox Ltd
+
+class jc_com_wordbridge extends JCommentsPlugin
+{
+ 
+    function getObjectTitle( $id ) {
+        // Data load from database by given id 
+        $db = & JFactory::getDBO();
+
+        // Need to get the blog uuid based on the current menu item
+        $app = JFactory::getApplication();
+        $menu = $app->getMenu();
+        $item = $menu->getActive();
+        if ( !$item )
+        {
+            $item = $menu->getItem( JCommentsPlugin::getItemid( 'com_wordbridge' ) );
+        }
+        $params = $item->params;
+        require_once( JPATH_COMPONENT.DS.'helpers'.DS.'helper.php' );
+        $blogInfo = WordbridgeHelper::getBlogByName( $params->get( 'wordbridge_blog_name' ) );
+        $db->setQuery( sprintf( "SELECT title FROM #__com_wordbridge_posts WHERE blog_uuid='%s' AND post_id = %d", $db->quote( $blogInfo['uuid'] ), $id % 10000000 ) );
+        return $db->loadResult();
+    }
+ 
+    function getObjectLink( $id ) {
+        // Itemid meaning of our component
+        $_Itemid = JCommentsPlugin::getItemid( 'com_wordbridge' );
+ 
+        // url link creation for given object by id 
+        $link = JRoute::_( 'index.php?option=com_wordbridge&task=view&p='. $id .'&Itemid='. $_Itemid );
+        return $link;
+    }
+ 
+    function getObjectOwner( $id ) {
+        $db = & JFactory::getDBO();
+        $db->setQuery( sprintf( "SELECT id, %s FROM #__user_usergroup_map AS map LEFT JOIN #__usergroups AS ug ON map.group_id = ug.id LEFT JOIN #__users AS u ON map.user_id = u.id WHERE u.block = 0 AND ug.id = 8 LIMIT 1", $db->quote( 'admin' ), $id ) );
+        return $db->loadResult();
+    }
+}
+
